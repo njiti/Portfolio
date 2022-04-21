c34e37dcf56c689400f99c42f73ab3a41114e33d
njiti <kamaukan77@gmail.com>
2022-04-21 12:05:28 +0300

    Qualifications
    Added a qualificztion section

--- main.js
+++ main.js
@@ -33,9 +33,9 @@ const skillsContent = document.getElementsByClassName('skills_content'),
        skillsHeader = document.querySelectorAll('.skills_header')
 
 function toggleSkills(){
-    let itemClass = this.parentNode.getElementsByClassName
+    let itemClass = this.parentNode.className
 
-    for(i = 0; i < skill.length; i++){
+    for(i = 0; i < skillsContent.length; i++){
         skillsContent[i].className = 'skills_content skills_close'
     }
     if(itemClass === 'skills_content skills_close'){
@@ -43,6 +43,32 @@ function toggleSkills(){
     }
 }
 
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
 
 /*QUALIFICATION TABS*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(data.database.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
+
 /*SERVICES MODAL*/
+/*PORTFOLIO SWIPER*/
+/*TESTIMONIAL*/
+/*SCROLL SECTIONS ACTIVE LINK*/
+/*CHANGE BACKGROUND HEADER*/
